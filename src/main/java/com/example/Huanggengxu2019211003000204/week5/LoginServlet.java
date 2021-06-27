ea64f98eadf834334330ba3d3c279ec2607fe7a0
2019211003000204Huanggengxu <320745780@qq.com>
2021-06-27 19:04:55 +0800

    Initial commit

---
+++ src/main/java/com/example/Huanggengxu2019211003000204/week5/LoginServlet.java
@@ -0,0 +1,70 @@
+package com.example.Huanggengxu2019211003000204.week5;
+
+import org.apache.commons.dbutils.QueryRunner;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        String driver = getServletConfig().getServletContext().getInitParameter("driver");
+        String url = getServletConfig().getServletContext().getInitParameter("url");
+        String username = getServletConfig().getServletContext().getInitParameter("username");
+        String password = getServletConfig().getServletContext().getInitParameter("password");
+
+        try{
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(url,username,password);
+            System.out.println("init()" + connection);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        String driver = getServletConfig().getServletContext().getInitParameter("driver");
+        String url = getServletConfig().getServletContext().getInitParameter("url");
+        String user = getServletConfig().getServletContext().getInitParameter("username");
+        String pd = getServletConfig().getServletContext().getInitParameter("password");
+
+        try {
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(url,user,pd);
+            String sql = "select * from usertable";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                String user1 = rs.getString(1);
+                String pd1 = rs.getString(2);
+                if(user1.equals(username) && pd1.equals(password)){
+                    PrintWriter printWriter =response.getWriter();
+                    printWriter.println("Login Success!!!");
+                    printWriter.println("Welcome " + username);
+                }else{
+                    PrintWriter printWriter =response.getWriter();
+                    printWriter.println("Login Error!");
+                }
+            }
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+}
